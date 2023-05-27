48a85a0039c06b4de2fb660518a85b849f82130e
sjain2580 <sjain040395@gmail.com>
2023-05-27 22:45:11 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pickle
+import requests
+import base64
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('movie.png') 
+
+def fetch_poster(movie_id):
+     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
+     data=requests.get(url)
+     data=data.json()
+     poster_path = data['poster_path']
+     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
+     return full_path
+
+movies = pickle.load(open("movies_list.pkl", 'rb'))
+similarity = pickle.load(open("similarity.pkl", 'rb'))
+movies_list=movies['title'].values
+
+
+st.header("Movie Recommender System")
+
+selectvalue=st.selectbox("Select movie from dropdown", movies_list)
+
+def recommend(movie):
+    index=movies[movies['title']==movie].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
+    recommend_movie=[]
+    recommend_poster=[]
+    for i in distance[1:4]:
+        movies_id=movies.iloc[i[0]].id
+        recommend_movie.append(movies.iloc[i[0]].title)
+        recommend_poster.append(fetch_poster(movies_id))
+    return recommend_movie, recommend_poster
+
+
+if st.button("Show Recommendations"):
+    st.subheader("These are the top 3 movie recommendations")
+    movie_name, movie_poster = recommend(selectvalue)
+    col1,col2,col3,col4,col5=st.columns(5)
+    with col1:
+        st.text(movie_name[0])
+        st.image(movie_poster[0])
+    with col2:
+        st.text(movie_name[1])
+        st.image(movie_poster[1])
+    with col3:
+        st.text(movie_name[2])
+        st.image(movie_poster[2])
+  
